8ad2ccab0cca3af269aa3bd6a4d4f12fd7a7fed2
iqbalmaulanaardi <iqbalmaulana.ardi@gmail.com>
2018-08-29 17:04:52 +0700

    w3x6 done

---
+++ w3/w3x5.js
@@ -0,0 +1,17 @@
+function palindrome(input) {
+    var output = '';
+    for (var index = input.length; index >= 1; index--) {
+        output += input[index - 1]
+    }
+    if (input === output) {
+        return true
+    } else {
+        return false
+    }
+}
+// TEST CASES
+console.log(palindrome('katak')); // true
+console.log(palindrome('blanket')); // false
+console.log(palindrome('civic')); // true
+console.log(palindrome('kasur rusak')); // true
+console.log(palindrome('mister')); // false
