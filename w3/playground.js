9231bca7f7e221ab7023bb95c7c886c8c96ce6fb
iqbalmaulanaardi <iqbalmaulana.ardi@gmail.com>
2018-09-04 14:14:14 +0700

    w3 done

--- w3/playground.js
+++ w3/playground.js
@@ -1,37 +1,58 @@
-function kalkulasi(statement) {
-    var angka1 = ''
-    var angka2 = ''
-    var tmp = ''
-    var result = 0;
-    for (var index = 0; index < statement.length; index++) {
-        if (statement[index] === '+' || statement[index] === '*' || statement[index] === '-' || statement[index] === '/') {
-            tmp = statement[index]
-        } else {
-            if (tmp === '') {
-                angka1 += statement[index]
-            } else if (tmp !== '') {
-                angka2 += statement[index]
-            }
-        }
-        switch (tmp) {
-            case '+':
-                result = Number(angka1) + Number(angka2)
-                break;
-            case '-':
-                result = Number(angka1) - Number(angka2)
-                break;
-            case '*':
-                result = Number(angka1) * Number(angka2)
-                break;
-            case '/':
-                result = Number(angka1) / Number(angka2)
-                break;
+// function kalkulasi(statement) {
+//     var angka1 = ''
+//     var angka2 = ''
+//     var tmp = ''
+//     var result = 0;
+//     for (var index = 0; index < statement.length; index++) {
+//         if (statement[index] === '+' || statement[index] === '*' || statement[index] === '-' || statement[index] === '/') {
+//             tmp = statement[index]
+//         } else {
+//             if (tmp === '') {
+//                 angka1 += statement[index]
+//             } else if (tmp !== '') {
+//                 angka2 += statement[index]
+//             }
+//         }
+//         switch (tmp) {
+//             case '+':
+//                 result = Number(angka1) + Number(angka2)
+//                 break;
+//             case '-':
+//                 result = Number(angka1) - Number(angka2)
+//                 break;
+//             case '*':
+//                 result = Number(angka1) * Number(angka2)
+//                 break;
+//             case '/':
+//                 result = Number(angka1) / Number(angka2)
+//                 break;
 
-            default:
-                break;
-        }
+//             default:
+//                 break;
+//         }
+//     }
+//     return result
+// }
+// console.log(kalkulasi('1+23'))
+// console.log(kalkulasi('2*22'))
+
+/*
+rows = 5
+*
+**
+***
+****
+*****
+*/
+var i = 0;
+var rows2 = 5;
+var str = "*"
+var j = 0
+while (i < rows2) {
+    while (j < i) {
+        str = str + "*";
+        j++;
     }
-    return result
+    console.log(str)
+    i++;
 }
-console.log(kalkulasi('1+23'))
-console.log(kalkulasi('2*22'))
